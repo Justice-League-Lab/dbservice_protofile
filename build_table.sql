36ac6ad0083868fd31fa711724b412c241ae9c0a
lubbock <dongzhi@hqshuke.com>
2024-04-24 13:55:57 +0800

    fix sql error

--- build_table.sql
+++ build_table.sql
@@ -1,25 +1,28 @@
 -- Role data : base info
 DROP TABLE IF EXISTS `role_data`;
 CREATE TABLE `role_data` (
-    `role_id`           bigint(20)      NOT NULL AUTO_INCREMENT,
+    `id`                bigint NOT NULL AUTO_INCREMENT COMMENT 'Primary key, should auto increment.',
+    `role_id`           bigint(20)      NOT NULL DEFAULT 0,
     `player_name`       varchar(64)     NOT NULL DEFAULT '',
     `account`           varchar(128)    NOT NULL DEFAULT '',
-    `equip_base_bin`    mediumblob      NOT NULL DEFAULT '',         
-    `equip_ext_bin`     mediumblob      NOT NULL DEFAULT '',
-    `item_bin`          mediumblob      NOT NULL DEFAULT '',
-    `pet_bin`           mediumblob      NOT NULL DEFAULT '',
+    `equip_base_bin`    mediumblob,         
+    `equip_ext_bin`     mediumblob,
+    `item_bin`          mediumblob,
+    `pet_bin`           mediumblob,
     `created_time`      datetime        NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_time`      datetime        NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-    PRIMARY KEY(`role_id`)
-) ENGINE = InnoDB AUTO_INCREMENT 10000 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+    `updated_time`      datetime        NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY(`id`),
+    KEY(`role_id`)
+) ENGINE = InnoDB AUTO_INCREMENT 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 -- Global data : activity1
 DROP TABLE IF EXISTS `activity_1`;
 CREATE TABLE `activity_1` (
-    `activity_data_id`  bigint(20)      NOT NULL AUTO_INCREMENT,
-    `role_id`           bigint(20)      NOT NULL,
-    `activity_bin`      mediumblob      NOT NULL DEFAULT '',
+    `id`                bigint          NOT NULL AUTO_INCREMENT COMMENT 'Primary key, should auto increment.',
+    `activity_data_id`  bigint(20)      NOT NULL DEFAULT 0,
+    `role_id`           bigint(20)      NOT NULL DEFAULT 0,
+    `activity_bin`      mediumblob,
     `created_time`      datetime        NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_time`      datetime        NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-    PRIMARY KEY(`activity_data_id`)
-) ENGINE = InnoDB AUTO_INCREMENT 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+    `updated_time`      datetime        NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY(`id`)
+) ENGINE = InnoDB AUTO_INCREMENT 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
